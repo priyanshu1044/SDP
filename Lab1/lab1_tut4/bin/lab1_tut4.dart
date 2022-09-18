98c125e54c32fd77e46a14368994b964182aa97d
priyanshu1044 <priyanshupatel1044@gmail.com>
2022-09-18 23:05:34 +0530

    lab1 done

---
+++ Lab1/lab1_tut4/bin/lab1_tut4.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+
+/*
+//program 1
+void main() {
+  print('Welcome students in the world of Dart & Flutter');
+}
+//output:Welcome students in the world of Dart & Flutter
+*/
+
+/*
+//program 2
+void main(){
+  int i;
+  for(i = 0;i < 6;i++)
+    print("hello ${i + 1}");
+}
+//output:
+// hello 1
+// hello 2
+// hello 3
+// hello 4
+// hello 5
+// hello 6
+*/
+
+/*
+//program 3
+void main() {
+  stdout.write('without new line ');
+  print('hello world');
+  print(calculate());
+}
+
+int calculate(){
+  return 7 * 7;
+}
+//output:without new line hellow world
+         49
+//print() function puts \n at the end of line but stdout.write doesn't puts \n at end of line.
+*/
---
+++ Lab1/lab1_tut4/test/lab1_tut4_test.dart
@@ -0,0 +1,8 @@
+import 'package:lab1_tut4/lab1_tut4.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
