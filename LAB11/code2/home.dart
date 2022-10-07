7e5199b339bb7150544ad0dc39e95cfa38f86df9
priyanshu1044 <70969437+priyanshu1044@users.noreply.github.com>
2022-10-07 13:52:55 +0530

    Add files via upload
    lab 11

---
+++ LAB11/code2/home.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  Map<dynamic,dynamic> data={};
+  @override
+  Widget build(BuildContext context) {
+    data=ModalRoute.of(context)?.settings.arguments as Map;
+    print(data);
+
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            TextButton.icon(onPressed: (){
+              Navigator.pushNamed(context,'/location');},
+                icon: Icon(Icons.edit_location),
+                label:Text('Edit Location'))
+          ],
+        ),
+      ),
+    );
+  }
+}
